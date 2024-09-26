eff024b47d92c0f0cdafbae7bea838fa15810bc9
thanawatJP <pjcasgaming@gmail.com>
2024-09-26 10:35:12 +0700

    update main
    - update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.html'],
+  content: [
+    './budgetbuddy/authen/templates/**/*.html', 
+    './budgetbuddy/account/templates/**/*.html'
+  ],
   theme: {
     extend: {},
   },
